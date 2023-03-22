aee477795524193ef3d73f2e2b9bc84e21be8457
James <james.ch.pino@gmail.com>
2023-03-22 10:57:17 -0700

    Adding custom tests for secretion data. Adding concerto dependency. Modifying github actions structure to be generic for additional repos.

--- rtoru/__init__.py
+++ rtoru/__init__.py
@@ -1,5 +1,7 @@
 import cobra
 import pathlib
+import pandas as pd
+
 
 path = pathlib.Path(__file__).parent
 
@@ -8,3 +10,8 @@ rt = cobra.io.read_sbml_model(
 )
 
 exp_file_path = path.joinpath('data', 'experiments.yml').__str__()
+expected_metab = pd.read_csv(
+    path.joinpath('data', 'excreted_rt.csv').__str__(),
+    index_col=0,
+
+).bigg_id
